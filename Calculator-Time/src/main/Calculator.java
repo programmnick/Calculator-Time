45dbae4570d92d2a7f8441a835ba3d9bcc85bfe2
Nikita <programmnick@gmail.com>
2019-03-20 23:05:55 +0300

    Code refactoring, fixing bugs

--- Calculator-Time/src/main/Calculator.java
+++ Calculator-Time/src/main/Calculator.java
@@ -9,18 +9,21 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class Calculator { 
+public class Calculator implements ActionListener { 
 	
 	String firstNum = "";
 	String secondNum = "";
 	String result ="";
 	int type = 0;
+    int ii = 0;
 	boolean isFirst = true;
 	boolean wasMath = false;
 	
 	JLabel main = new JLabel("");   
     JLabel typeL = new JLabel("");
     JLabel top = new JLabel("");
+    
+    JButton[] buttons;
     	
     static JFrame frame = new JFrame();
     
@@ -40,21 +43,21 @@ public class Calculator {
      public void setupGUI() {
          
         JPanel p = new JPanel(); 
-        JPanel s = new JPanel();
+        JPanel s = new JPanel();      
         
         main.setBounds(10,15,200,40);
-        main.setFont(new Font("dsds", Font.BOLD, 29));
+        main.setFont(new Font("font", Font.BOLD, 29));
         
         top.setBounds(26,1,200,14);
-        top.setFont(new Font("dsds", Font.PLAIN, 14));
+        top.setFont(new Font("font", Font.PLAIN, 14));
         
         typeL.setBounds(16,1,20,14);
-        typeL.setFont(new Font("dsds", Font.PLAIN, 12));
-        s.setPreferredSize(new Dimension(200,55));
+        typeL.setFont(new Font("font", Font.PLAIN, 12));
         
         p.setLayout(new GridLayout(4,4,3,3));
-        s.setLayout(null);
-               
+        
+        s.setPreferredSize(new Dimension(200,55));
+        s.setLayout(null);            
         s.add(main);   
         s.add(top);
         s.add(typeL);
@@ -62,33 +65,20 @@ public class Calculator {
         frame.getContentPane().add(BorderLayout.NORTH, s);     
         frame.getContentPane().add(BorderLayout.CENTER, p); 
                
-        JButton sev = new JButton("7"); 
-        sev.setBackground(Color.WHITE);
-        sev.setFont(new Font("dsds", Font.BOLD, 19));
-        sev.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "7"; main.setText(firstNum);} else {secondNum += "7";  main.setText(secondNum);}   	
-			}});					
-
-        JButton eig = new JButton("8"); 
-        eig.setBackground(Color.WHITE);
-        eig.setFont(new Font("dsds", Font.BOLD, 19));
-        eig.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "8"; main.setText(firstNum);} else {secondNum += "8";  main.setText(secondNum);}   	
-			}});					
-
-        JButton nin = new JButton("9");
-        nin.setBackground(Color.WHITE);
-        nin.setFont(new Font("dsds", Font.BOLD, 19));
-        nin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "9"; main.setText(firstNum);} else {secondNum += "9";  main.setText(secondNum);}   	
-			}});					
-
-        JButton del = new JButton("<="); 
-        del.setBackground(new Color(240,240,240));
-        del.addActionListener(new ActionListener() {
+        buttons = new JButton[10];
+        
+        for (int i = 0; i < 10; i++) {
+        	ii = i;
+        	buttons[i]  = new JButton(String.valueOf(i));
+        	buttons[i].setBackground(Color.WHITE);
+        	buttons[i].setFont(new Font("font", Font.BOLD, 19));
+        	buttons[i].addActionListener(this);
+        }           
+        
+       
+        JButton delete = new JButton("<="); 
+        delete.setBackground(new Color(240,240,240));
+        delete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) { 
 				if (result.length() > 0) { result = result.substring(0, result.length()-1); main.setText(result); }
 				  if (isFirst && firstNum.length() > 0) {			  
@@ -100,128 +90,74 @@ public class Calculator {
 				  }
 			}}});				     
              
-        JButton fou = new JButton("4"); 
-        fou.setBackground(Color.WHITE);
-        fou.setFont(new Font("dsds", Font.BOLD, 19));
-        fou.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "4"; main.setText(firstNum);} else {secondNum += "4";  main.setText(secondNum);}   	
-			}});					
-
-        JButton fiv = new JButton("5"); 
-        fiv.setBackground(Color.WHITE);
-        fiv.setFont(new Font("dsds", Font.BOLD, 19));
-        fiv.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "5"; main.setText(firstNum);} else {secondNum += "5";  main.setText(secondNum);}   	
-			}});					
-
-        JButton six = new JButton("6");
-        six.setBackground(Color.WHITE);
-        six.setFont(new Font("dsds", Font.BOLD, 19));
-        six.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "6"; main.setText(firstNum);} else {secondNum += "6";  main.setText(secondNum);}   	
-			}});					
-
-        JButton multi = new JButton("X");
-        multi.setBackground(new Color(240,240,240));
-        multi.addActionListener(new ActionListener() {
+        
+        JButton multiply = new JButton("X");
+        multiply.setBackground(new Color(240,240,240));
+        multiply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) { 
 				if (result.length() > 0) {
-					  type = 3;				 					  
-					  typeL.setText("x");
-					  isFirst = false;
-					  firstNum = "";
-					  firstNum += result;
-					  top.setText(firstNum);
+					type = 3;				 					  
+					typeL.setText("x");
+					isFirst = false;
+					firstNum = "";
+					firstNum += result;
+					top.setText(firstNum);
 				}
-				  type = 3;
-				  typeL.setText("x");
-				  isFirst = false;
-				  top.setText(firstNum);
+				type = 3;
+				typeL.setText("x");
+				isFirst = false;
+				top.setText(firstNum);
 			}});				
         
-        JButton one = new JButton("1");
-        one.setBackground(Color.WHITE);
-        one.setFont(new Font("dsds", Font.BOLD, 19));      
-        one.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "1"; main.setText(firstNum);} else {secondNum += "1";  main.setText(secondNum);}   	
-			}});					
-
-        JButton two = new JButton("2");
-        two.setBackground(Color.WHITE);
-        two.setFont(new Font("dsds", Font.BOLD, 19));
-        two.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "2"; main.setText(firstNum);} else {secondNum += "2";  main.setText(secondNum);}   	
-			}});					
-
-        JButton thr = new JButton("3");
-        thr.setBackground(Color.WHITE);
-        thr.setFont(new Font("dsds", Font.BOLD, 19));
-        thr.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "3"; main.setText(firstNum);} else {secondNum += "3";  main.setText(secondNum);}   	
-			}});					
-
+        
         JButton divide = new JButton("/");
         divide.setBackground(new Color(240,240,240));
         divide.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) { 
 				if (result.length() > 0) {
-					  type = 4;				 					  
-					  typeL.setText("/");
-					  isFirst = false;
-					  firstNum = "";
-					  firstNum += result;
-					  top.setText(firstNum);
+					type = 4;				 					  
+					typeL.setText("/");
+					isFirst = false;
+					firstNum = "";
+					firstNum += result;
+					top.setText(firstNum);
 				}
-				  type = 4;
-				  typeL.setText("/");
-				  isFirst = false;
-				  top.setText(firstNum);
+				type = 4;
+				typeL.setText("/");
+				isFirst = false;
+				top.setText(firstNum);
 			}});				     
         
-        JButton min = new JButton("-");
-        min.setBackground(new Color(240,240,240));
-        min.addActionListener(new ActionListener() {
+        JButton minus = new JButton("-");
+        minus.setBackground(new Color(240,240,240));
+        minus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) { 
 				if (result.length() > 0) {
-					  type = 2;				 					  
-					  typeL.setText("-");
-					  isFirst = false;
-					  firstNum = "";
-					  firstNum += result;
-					  top.setText(firstNum);
+					type = 2;				 					  
+					typeL.setText("-");
+					isFirst = false;
+					firstNum = "";
+					firstNum += result;
+					top.setText(firstNum);
 				}
-				  type = 2;
-				  typeL.setText("-");
-				  isFirst = false;
-				  top.setText(firstNum);
+				type = 2;
+				typeL.setText("-");
+				isFirst = false;
+				top.setText(firstNum);
 			}});				     
-        
-        JButton zer = new JButton("0");       
-        zer.setBackground(Color.WHITE);
-        zer.setFont(new Font("dsds", Font.BOLD, 19));
-        zer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (isFirst) {result = ""; firstNum += "0"; main.setText(firstNum);} else {secondNum += "0";  main.setText(secondNum);}   	
-			}});					
-
-        JButton plu = new JButton("+");  
-        plu.setBackground(new Color(240,240,240));
-        plu.addActionListener(new ActionListener() {
+              
+        JButton plus = new JButton("+");  
+        plus.setBackground(new Color(240,240,240));
+        plus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) { 
 				if (result.length() > 0) {					  					  
 					firstNum = "";
 					firstNum += result;					  
 				}
-				  type = 1;				 
-				  typeL.setText("+");
-				  isFirst = false;
-				  top.setText(firstNum);
+				type = 1;				 
+				typeL.setText("+");
+				isFirst = false;
+				top.setText(firstNum);
 			}});				
       
         JButton count = new JButton("="); 
@@ -229,58 +165,66 @@ public class Calculator {
         count.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (type == 0) {
-					top.setText("Выберите что-нибудь");					
-				}
-				
-				if (result.length() > 0 && secondNum.equals("") ) {
-					secondNum += result;
-				}
-				
-				if (secondNum.equals("") && type > 0) secondNum += "0";
-								
-					if (type == 1) {
-						result = "";
-						result += Integer.valueOf(firstNum) + Integer.valueOf(secondNum);
-						main.setText(result); top.setText(""); typeL.setText(""); firstNum = ""; secondNum = ""; isFirst = true; type = 0;	
+					top.setText("Choose mathtype");		
+					return;
 				}				
-					if (type == 2) {
-						result = "";
-						result += Integer.valueOf(firstNum) - Integer.valueOf(secondNum);
-						main.setText(result); top.setText(""); typeL.setText(""); firstNum = ""; secondNum = ""; isFirst = true; type = 0;
-				}
-					
-					if (type == 3) {
-						result = "";
-						result += Integer.valueOf(firstNum) * Integer.valueOf(secondNum);
-						main.setText(result); top.setText(""); typeL.setText(""); firstNum = ""; secondNum = ""; isFirst = true; type = 0;			
-				}
-					
-					if (type == 4) {
-						result = "";
-						result += Integer.valueOf(firstNum) / Integer.valueOf(secondNum);
-						main.setText(result); top.setText(""); typeL.setText(""); firstNum = ""; secondNum = ""; isFirst = true; type = 0;
-						}
-							
+				if (secondNum.equals("") && result.length() > 0) secondNum += result;
+				if (secondNum.equals("") && type > 0) secondNum = firstNum;
+				
+				result = ""; 
+				
+				if (type == 1) result += Integer.valueOf(firstNum) + Integer.valueOf(secondNum);		
+				if (type == 2) result += Integer.valueOf(firstNum) - Integer.valueOf(secondNum);					
+				if (type == 3) result += Integer.valueOf(firstNum) * Integer.valueOf(secondNum);
+				if (type == 4) result += Integer.valueOf(firstNum) / Integer.valueOf(secondNum);
+				
+				main.setText(result);
+				top.setText("");
+				typeL.setText("");
+				firstNum = "";
+				secondNum = "";
+				isFirst = true;
+				type = 0;
 				System.out.println("result - " + result);
-				} });
+			} 
+		});
                                            
-        p.add(sev); 
-        p.add(eig); 
-        p.add(nin);
-        p.add(del); 
-        p.add(fou); 
-        p.add(fiv); 
-        p.add(six); 
-        p.add(multi); 
-        p.add(one); 
-        p.add(two); 
-        p.add(thr); 
+        p.add(buttons[7]);
+        p.add(buttons[8]);
+        p.add(buttons[9]);
+        p.add(delete); 
+        p.add(buttons[4]); 
+        p.add(buttons[5]);
+        p.add(buttons[6]); 
+        p.add(multiply); 
+        p.add(buttons[1]); 
+        p.add(buttons[2]);
+        p.add(buttons[3]);
         p.add(divide); 
-        p.add(min);
-        p.add(zer);          
-        p.add(plu);     
+        p.add(minus);
+        p.add(buttons[0]);         
+        p.add(plus);     
         p.add(count); 
-               
+     
     }
+        
+     
+	public void actionPerformed(ActionEvent e) {	
+		int source = Integer.parseInt(e.getActionCommand());
+		System.out.println(source);
+		
+		if (source >= 0 && source <= 9) {
+			if (isFirst) {
+				result = ""; 
+				firstNum += String.valueOf(source); 
+				main.setText(firstNum);
+			} else {
+				secondNum += String.valueOf(source) ;  
+				main.setText(secondNum);
+			}
+			return;
+		}				
+	
+	}
 }
 

b2f4969629509cbf1bb9165d628fb8a00913ee80
Nikita <programmnick@gmail.com>
2019-03-21 18:23:43 +0300

    Code refactoring & optimization

--- Calculator-Time/src/main/Calculator.java
+++ Calculator-Time/src/main/Calculator.java
@@ -13,9 +13,11 @@ public class Calculator implements ActionListener {
 	
 	String firstNum = "";
 	String secondNum = "";
-	String result ="";
+	String result = "";
+	
 	int type = 0;
-    int ii = 0;
+	int source = 0;
+	
 	boolean isFirst = true;
 	boolean wasMath = false;
 	
@@ -24,10 +26,10 @@ public class Calculator implements ActionListener {
     JLabel top = new JLabel("");
     
     JButton[] buttons;
+    String[] buttonNames = {"0","1","2","3","4","5","6","7","8","9","←","x","/","-","+","CE","C","+-",",",};
     	
     static JFrame frame = new JFrame();
-    
-    
+     
     public static void main(String[] args) { 
            	 
    	 	frame.setTitle("CalcTime");	
@@ -40,8 +42,7 @@ public class Calculator implements ActionListener {
         cal.setupGUI();
     }
            
-     public void setupGUI() {
-         
+     public void setupGUI() {         
         JPanel p = new JPanel(); 
         JPanel s = new JPanel();      
         
@@ -65,101 +66,23 @@ public class Calculator implements ActionListener {
         frame.getContentPane().add(BorderLayout.NORTH, s);     
         frame.getContentPane().add(BorderLayout.CENTER, p); 
                
-        buttons = new JButton[10];
-        
+        buttons = new JButton[19];
+         
         for (int i = 0; i < 10; i++) {
-        	ii = i;
-        	buttons[i]  = new JButton(String.valueOf(i));
+        	buttons[i] = new JButton(String.valueOf(i));
         	buttons[i].setBackground(Color.WHITE);
         	buttons[i].setFont(new Font("font", Font.BOLD, 19));
         	buttons[i].addActionListener(this);
         }           
         
-       
-        JButton delete = new JButton("<="); 
-        delete.setBackground(new Color(240,240,240));
-        delete.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (result.length() > 0) { result = result.substring(0, result.length()-1); main.setText(result); }
-				  if (isFirst && firstNum.length() > 0) {			  
-					  firstNum = firstNum.substring(0, firstNum.length()-1);
-					  main.setText(firstNum);
-					  if (!isFirst && firstNum.length() > 0) {
-					  secondNum = secondNum.substring(0, secondNum.length()-1);
-					  main.setText(secondNum);
-				  }
-			}}});				     
-             
-        
-        JButton multiply = new JButton("X");
-        multiply.setBackground(new Color(240,240,240));
-        multiply.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (result.length() > 0) {
-					type = 3;				 					  
-					typeL.setText("x");
-					isFirst = false;
-					firstNum = "";
-					firstNum += result;
-					top.setText(firstNum);
-				}
-				type = 3;
-				typeL.setText("x");
-				isFirst = false;
-				top.setText(firstNum);
-			}});				
-        
-        
-        JButton divide = new JButton("/");
-        divide.setBackground(new Color(240,240,240));
-        divide.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (result.length() > 0) {
-					type = 4;				 					  
-					typeL.setText("/");
-					isFirst = false;
-					firstNum = "";
-					firstNum += result;
-					top.setText(firstNum);
-				}
-				type = 4;
-				typeL.setText("/");
-				isFirst = false;
-				top.setText(firstNum);
-			}});				     
-        
-        JButton minus = new JButton("-");
-        minus.setBackground(new Color(240,240,240));
-        minus.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (result.length() > 0) {
-					type = 2;				 					  
-					typeL.setText("-");
-					isFirst = false;
-					firstNum = "";
-					firstNum += result;
-					top.setText(firstNum);
-				}
-				type = 2;
-				typeL.setText("-");
-				isFirst = false;
-				top.setText(firstNum);
-			}});				     
-              
-        JButton plus = new JButton("+");  
-        plus.setBackground(new Color(240,240,240));
-        plus.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) { 
-				if (result.length() > 0) {					  					  
-					firstNum = "";
-					firstNum += result;					  
-				}
-				type = 1;				 
-				typeL.setText("+");
-				isFirst = false;
-				top.setText(firstNum);
-			}});				
-      
+        for (int i = 10; i < 19; i++) {
+        	buttons[i] = new JButton(buttonNames[i]);
+        	buttons[i].setBackground(new Color(240,240,240)); 
+        	buttons[i].setFont(new Font("font", Font.BOLD, 19));
+        	buttons[i].addActionListener(this);
+        }                            				     
+                     
+   
         JButton count = new JButton("="); 
         count.setBackground(new Color(240,240,240));
         count.addActionListener(new ActionListener() {
@@ -173,10 +96,16 @@ public class Calculator implements ActionListener {
 				
 				result = ""; 
 				
-				if (type == 1) result += Integer.valueOf(firstNum) + Integer.valueOf(secondNum);		
-				if (type == 2) result += Integer.valueOf(firstNum) - Integer.valueOf(secondNum);					
-				if (type == 3) result += Integer.valueOf(firstNum) * Integer.valueOf(secondNum);
-				if (type == 4) result += Integer.valueOf(firstNum) / Integer.valueOf(secondNum);
+				if (type == 14) result += Double.valueOf(firstNum) + Double.valueOf(secondNum);		
+				if (type == 13) result += Double.valueOf(firstNum) - Double.valueOf(secondNum);					
+				if (type == 11) result += Double.valueOf(firstNum) * Double.valueOf(secondNum);
+				if (type == 12) result += Double.valueOf(firstNum) / Double.valueOf(secondNum);							
+				
+				if ((result.substring(result.length()-2,result.length()).equals(".0"))) {					
+					result = result.substring(0,result.length()-2);
+				}
+
+				System.out.println(firstNum + " " + secondNum + " " + result);
 				
 				main.setText(result);
 				top.setText("");
@@ -185,35 +114,84 @@ public class Calculator implements ActionListener {
 				secondNum = "";
 				isFirst = true;
 				type = 0;
-				System.out.println("result - " + result);
 			} 
 		});
                                            
         p.add(buttons[7]);
         p.add(buttons[8]);
         p.add(buttons[9]);
-        p.add(delete); 
+        p.add(buttons[10]);        
         p.add(buttons[4]); 
         p.add(buttons[5]);
-        p.add(buttons[6]); 
-        p.add(multiply); 
+        p.add(buttons[6]);
+        p.add(buttons[11]);
         p.add(buttons[1]); 
         p.add(buttons[2]);
         p.add(buttons[3]);
-        p.add(divide); 
-        p.add(minus);
+        p.add(buttons[12]); 
+        p.add(buttons[13]);
         p.add(buttons[0]);         
-        p.add(plus);     
-        p.add(count); 
-     
+        p.add(buttons[14]);     
+        p.add(count);      
     }
-        
-     
+           
 	public void actionPerformed(ActionEvent e) {	
-		int source = Integer.parseInt(e.getActionCommand());
-		System.out.println(source);
+		for (int i = 0; i < 16; i++) {
+			if (e.getActionCommand().equals(buttonNames[i])) {
+				source = i;
+			}
+		}			
 		
-		if (source >= 0 && source <= 9) {
+		switch (source) {
+		case 10: 
+			if (result.length() > 0) { 
+				result = result.substring(0, result.length()-1); main.setText(result); 
+				
+			}
+			if (isFirst && firstNum.length() > 0) {			  
+				firstNum = firstNum.substring(0, firstNum.length()-1);
+				main.setText(firstNum);
+			}
+			if (!isFirst && firstNum.length() > 0) {
+				secondNum = secondNum.substring(0, secondNum.length()-1);
+				main.setText(secondNum);			
+			}
+			return;
+
+		case 16: 
+			firstNum = "";
+			secondNum = "";
+			isFirst = true;
+			type = 0;
+			main.setText(firstNum);
+			top.setText(secondNum);
+			typeL.setText("");
+			return;
+		
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+			if (result.length() > 0) {					  					  
+				firstNum = "";
+				firstNum += result;					  
+			}			
+			type = source;				 
+			typeL.setText(buttonNames[source]);
+			isFirst = false;
+			top.setText(firstNum); 
+			return;
+			
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
 			if (isFirst) {
 				result = ""; 
 				firstNum += String.valueOf(source); 
@@ -221,10 +199,11 @@ public class Calculator implements ActionListener {
 			} else {
 				secondNum += String.valueOf(source) ;  
 				main.setText(secondNum);
-			}
+			}		
 			return;
-		}				
-	
-	}
+		}								
+		
+	}	
 }
+		
 
